c70aad0c11de6d0d75d7c1f68c49cbd40049ab33
Gabriel Cervo <joaogabrieldouradocervo@gmail.com>
2020-10-21 16:05:26 -0300

    JS code cleanup

---
+++ scripts/filtroListas.js
@@ -0,0 +1,35 @@
+const filter1 = document.getElementById("filters").getAttribute("data-filter1");
+const filter2 = document.getElementById("filters").getAttribute("data-filter2");
+const filters = [filter1, filter2];
+
+function applyFilters() {
+  let query = "";
+
+  for (const filter of filters) {
+    let checked = '';
+
+    $(`input[name="${filter}"]:checked`).each(function(){
+      checked += `${$(this).val()}, `;
+    });
+    
+    checked = checked.trim().slice(0, -1);
+
+    if (checked !== '') {        
+      query += `[data-${filter}*="${checked}"]`;
+    }
+
+  }
+
+  $(`div.filter-card:not(${query})`).hide();
+  $(`div.filter-card${query}`).show();
+
+  const visibleCards = $('div.filter-card:visible').length;
+
+  $('div.filter-card-empty')[0].style.display = visibleCards === 0 ? '' : 'none';
+}
+
+const input = $('input.filter')
+
+input.change(function(){
+  applyFilters()
+});
---
+++ scripts/modal.js
@@ -0,0 +1,33 @@
+// Gets all modals in the page
+const modals = document.querySelectorAll('.modal');
+
+// Get the button that opens the modal and adds the onClick event for all buttons */
+document.querySelectorAll('.open-modal').forEach(btn => {
+    btn.addEventListener('click', e => handleOpenModal(e));
+});
+
+// Get the <span> element that closes the modal
+document.querySelectorAll('.close').forEach(span => {
+  span.addEventListener('click', e => handleCloseModal(e));
+});
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (e) => {
+  if (e.target.classList.contains('modal')) {
+    handleCloseModal(e);
+  }
+}
+
+function handleOpenModal(e) {
+  console.log(e);
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+}
+
+function handleCloseModal(e) {
+  for (const index in modals) {
+    modals[index].style.display = typeof modals[index].style !== undefined && "none";
+  }
+}
